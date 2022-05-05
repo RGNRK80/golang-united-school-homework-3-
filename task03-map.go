7edcd10fcabd5fdf598eb28bb3646bb3cd84b34e
rgnrk80 <armhound@gmail.com>
2022-05-05 22:25:09 +0300

    3task

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,27 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
-	return
+	arKey := make([]int, 0, 0)
+
+	for k, _ := range input {
+		arKey = append(arKey, k)
+		//fmt.Println(k)
+
+	}
+	sort.Ints(arKey)
+	//fmt.Println(arKey)
+
+	arStr := make([]string, 0, 0)
+
+	for i := 0; i < len(input); i++ {
+		arStr = append(arStr, input[arKey[i]])
+	}
+
+	//Place your code here
+	return arStr
 }
